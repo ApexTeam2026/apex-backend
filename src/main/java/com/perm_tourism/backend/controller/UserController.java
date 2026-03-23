8387994654877e5c1204bd6e434c9e7848f92e16
Ekaterina Sivkova <Katasivkova352@gmail.com>
2026-03-23 23:46:23 +0500

    Добавлен запрос для регистрации пользователя

--- src/main/java/com/perm_tourism/backend/controller/UserController.java
+++ src/main/java/com/perm_tourism/backend/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.perm_tourism.backend.controller;
 
+import com.perm_tourism.backend.dto.UserRegistrationDto;
+import com.perm_tourism.backend.dto.UserResponseDto;
+import com.perm_tourism.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    // Регистрация пользователя
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> register (@Valid @RequestBody UserRegistrationDto dto) {
+        try {
+            UserResponseDto registeredUser = userService.register(dto);
+            return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Пользователь с таким email уже существует
+        }
+    }
 }
