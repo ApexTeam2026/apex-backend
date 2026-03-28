4788051b0d99885bf51cefeed3425b7b835632bd
GolyakovaKseniya <golyakovakseniya@mail.ru>
2026-03-28 21:32:50 +0500

    Добавлены проверки для пользователя

--- src/main/java/com/perm_tourism/backend/dto/UserRegistrationDto.java
+++ src/main/java/com/perm_tourism/backend/dto/UserRegistrationDto.java
@@ -8,23 +8,23 @@ import java.time.LocalDate;
 
 @Data
 public class UserRegistrationDto {
-    @Pattern(regexp = "^[a-zA-Zа-яА-Я\\s-]+$", message = "Имя пользователя может содержать только буквы, пробелы и дефисы")
+    @Pattern(regexp= "^[a-zA-Zа-яА-Я\\s-]+$", message = "Имя пользователя может содержать только буквы, пробелы и дефисы")
     @NotBlank(message = "Имя пользователя обязательно")
     @Size(min = 2,max = 100, message = "Имя пользователя должно быть от 2 до 100 символов")
     private String name;
 
-    @Pattern(regexp= "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$", message = "Email должен соответствовать стандартному формату")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$", message = "Email должен соответствовать стандартному формату")
     @NotBlank(message = "Email обязателен")
     @Email(message = "Некорректный email")
     private String email;
 
-    @Pattern(regexp= "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Пароль должен содержать минимум одну цифру, одну заглавную букву, " +
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Пароль должен содержать минимум одну цифру, одну заглавную букву, " +
             "одну строчную, один спецсимвол и не содержать пробелов")
     @NotBlank(message = "Пароль обязателен")
     @Size(min = 8, max= 100, message = "Пароль должен содержать от 8 до 100 символов")
     private String password;
 
-    @DateTimeFormat(pattern= "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "Дата рождения обязательна")
     @Past(message = "Указана некорректная дата рождения")
     private LocalDate birthdayDate;
